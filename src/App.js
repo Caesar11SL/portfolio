a4711d30912f41fbd26dc61f7ded02483652f2fb
Caesar11SL <97268174+Caesar11SL@users.noreply.github.com>
2022-07-24 18:43:15 -0700

    starting project

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import NavBar from './components/NavBar';
 import Homepage from './components/HomePage';
 import AboutMe from './components/AboutMe';
 import Projects from './components/Projects';
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Route, Routes, Router, Link } from "react-router-dom";
 
 
 function App() {
   return (
     <>
+    <Router>
     <NavBar />
-    {/* <Routes>
-    <Route path="/" exact component={ <Homepage /> } />
-    <Route path="/aboutme" exact component={ <AboutMe /> } />
-    <Route path="/projects" exact component={ <Projects /> } />
-    </Routes> */}
+      <Routes>
+        <Route path="/" exact component={ <Homepage /> } />
+        <Route path="/aboutme" exact component={ <AboutMe /> } />
+        <Route path="/projects" exact component={ <Projects /> } />
+      </Routes>
+    </Router>
     </>
   );
 }
